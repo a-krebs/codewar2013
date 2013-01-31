476de75056f540c968eb8823bbb730bf202b7b9f
Brent Erickson <brent2737@gmail.com>
2013-01-30 21:09:25 -0700

    Fix an edge case where there are no controls at a 3-way intersection

--- AI/PathFinder.cs
+++ AI/PathFinder.cs
@@ -22,6 +22,7 @@ namespace PlayerCSharpAI.AI
         {
             Point currentPoint = Point.Empty, previousPoint = Point.Empty, firstPoint = Point.Empty;
             MapSquare square, firstSquare, previousSquare, currentSquare;
+            MapSquare square, firstSquare, previousSquare, currentSquare, perpSquare1, perpSquare2;
             double pathScore, calculatedSpeed;
             for (int i = 0; i < map.Width; i++)
             {
@@ -41,6 +42,8 @@ namespace PlayerCSharpAI.AI
                     }
                     for (int k = j + 1; k < map.Height; k++)
                     {
+                        perpSquare1 = map.SquareOrDefault(new Point(i + 1, k));
+                        perpSquare2 = map.SquareOrDefault(new Point(Math.Max(0, i - 1), k));
                         previousPoint = new Point(i, k - 1);
                         previousSquare = map.SquareOrDefault(previousPoint);
                         currentPoint = new Point(i, k);
@@ -53,7 +56,7 @@ namespace PlayerCSharpAI.AI
                         }
                         calculatedSpeed = Math.Max(MIN_SPEED, Math.Min(calculatedSpeed + 0.1, MAX_SPEED));
                         pathScore += calculatedSpeed;
-                        if (currentSquare.Signal || currentSquare.StopSigns != MapSquare.STOP_SIGNS.NONE)
+                        if (currentSquare.Signal || currentSquare.StopSigns != MapSquare.STOP_SIGNS.NONE || (perpSquare1 != null && perpSquare1.IsDriveable) || (perpSquare2 != null && perpSquare2.IsDriveable))
                         {
                             paths.Add(new Path(firstPoint, currentPoint, pathScore / (currentPoint.Y - firstPoint.Y + currentPoint.X - firstPoint.X)));
                             if (currentSquare.StopSigns == MapSquare.STOP_SIGNS.STOP_NORTH || currentSquare.StopSigns == MapSquare.STOP_SIGNS.STOP_SOUTH)
@@ -86,6 +89,8 @@ namespace PlayerCSharpAI.AI
                     }
                     for (int k = j + 1; k < map.Width; k++)
                     {
+                        perpSquare1 = map.SquareOrDefault(new Point(k, i + 1));
+                        perpSquare2 = map.SquareOrDefault(new Point(k, Math.Max(0, i - 1)));
                         previousPoint = new Point(k - 1, i);
                         previousSquare = map.SquareOrDefault(previousPoint);
                         currentPoint = new Point(k, i);
@@ -98,7 +103,7 @@ namespace PlayerCSharpAI.AI
                         }
                         calculatedSpeed = Math.Max(MIN_SPEED, Math.Min(calculatedSpeed + 0.1, MAX_SPEED));
                         pathScore += calculatedSpeed;
-                        if (currentSquare.Signal || currentSquare.StopSigns != MapSquare.STOP_SIGNS.NONE)
+                        if (currentSquare.Signal || currentSquare.StopSigns != MapSquare.STOP_SIGNS.NONE || (perpSquare1 != null && perpSquare1.IsDriveable) || (perpSquare2 != null && perpSquare2.IsDriveable))
                         {
                             paths.Add(new Path(firstPoint, currentPoint, pathScore / (currentPoint.Y - firstPoint.Y + currentPoint.X - firstPoint.X)));
                             if (currentSquare.StopSigns == MapSquare.STOP_SIGNS.STOP_EAST || currentSquare.StopSigns == MapSquare.STOP_SIGNS.STOP_WEST)
